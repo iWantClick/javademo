3ab02ce7c01528795c455b00d55290d4a909e449
tony <908601756@qq.com>
2015-01-01 18:45:56 +0800

    build maven project

---
+++ src/main/java/com/iwc/javademo/Main.java
@@ -0,0 +1,10 @@
+package com.iwc.javademo;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("start");
+
+	}
+}
---
+++ src/test/java/com/iwc/javademo/MainTest.java
@@ -0,0 +1,10 @@
+package com.iwc.javademo;
+
+public class MainTest
+{
+	public static void main(String[] args)
+	{
+		System.out.println("start");
+
+	}
+}
